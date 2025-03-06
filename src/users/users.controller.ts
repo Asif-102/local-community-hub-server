1b73d4866f4d8b22c8ad636458509cadd84d3d41
Asif-102 <asif1zero2@gmail.com>
2025-03-06 16:45:22 +0600

    update route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,15 @@
 import { Controller, Get, HttpCode, HttpStatus, ParseIntPipe, UseGuards } from "@nestjs/common";
-import { Roles } from "src/auth/decorators/roles.decorator";
-import { Role } from "src/auth/enums/role.enum";
 import { JwtAccessGuard } from "src/auth/guards/jwt-access.guard";
-import { RolesGuard } from "src/auth/guards/roles/roles.guard";
 import { CurrentUser } from "src/utils/decorators/current-user.decorator";
 import { UsersService } from "./users.service";
 
-@UseGuards(RolesGuard)
+// @UseGuards(RolesGuard)
 @UseGuards(JwtAccessGuard)
 @Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Roles(Role.USER, Role.ADMIN, Role.SUPER_ADMIN)
+  // @Roles(Role.USER, Role.ADMIN, Role.SUPER_ADMIN)
   @Get("me")
   @HttpCode(HttpStatus.OK)
   fetchMe(@CurrentUser("id", ParseIntPipe) id: number) {
